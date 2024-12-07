80ae5cbe22467c5d097f3c6ee26082c22905043f
German Fica <germafica@gmail.com>
2024-12-07 00:40:25 -0300

    Add ensureDirectoryExists in qrcode-generator.js

--- src/qrcode-generator.js
+++ src/qrcode-generator.js
@@ -1,4 +1,17 @@
+import fs from 'fs';
+import path from 'path';
 import QRCode from 'qrcode';
+
+/**
+ * Ensures that the output directory exists. Creates it if it doesn't.
+ * @param {string} dirPath - The directory path to check or create.
+ */
+const ensureDirectoryExists = (dirPath) => {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+    console.log(`Directory created: ${dirPath}`);
+  }
+};
 
 /**
  * Generates a QR Code with the given configuration and saves it to a file.
@@ -8,6 +21,9 @@ import QRCode from 'qrcode';
  */
 export const generateQRCode = async (data, options, outputPath) => {
   try {
+    const outputDir = path.dirname(outputPath);
+    ensureDirectoryExists(outputDir);
+
     await QRCode.toFile(outputPath, data, options);
     console.log(`QR Code generated and saved to ${outputPath}`);
   } catch (error) {
